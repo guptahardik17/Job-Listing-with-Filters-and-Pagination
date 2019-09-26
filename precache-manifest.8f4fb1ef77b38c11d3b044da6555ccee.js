fe4791cffa2db0f90afaf78332513509c4b11339
guptahardik17 <hardik-dockabl@hardik-dockabls-MacBook-Air.local>
2019-09-26 11:01:02 +0530

    Updates

---
+++ precache-manifest.8f4fb1ef77b38c11d3b044da6555ccee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b9d397d76f1acf87dab",
+    "url": "/Job-Listing-with-Filters-and-Pagination/static/js/runtime~main.6b10f9ec.js"
+  },
+  {
+    "revision": "d2c0ed1cfb8686686207",
+    "url": "/Job-Listing-with-Filters-and-Pagination/static/js/main.5d13b92b.chunk.js"
+  },
+  {
+    "revision": "495d3448a1934e2f8450",
+    "url": "/Job-Listing-with-Filters-and-Pagination/static/js/2.9222f468.chunk.js"
+  },
+  {
+    "revision": "d2c0ed1cfb8686686207",
+    "url": "/Job-Listing-with-Filters-and-Pagination/static/css/main.2fed3e5a.chunk.css"
+  },
+  {
+    "revision": "4c118df9c880ea2fae9f74d6934a1e79",
+    "url": "/Job-Listing-with-Filters-and-Pagination/index.html"
+  }
+];
